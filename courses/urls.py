db8b657d2d5aa5b91076551841466ee35e5f3bba
Ravique <dofin-agas@ya.ru>
2019-06-07 14:49:05 +0300

    - urls fixes + courses in account details

--- courses/urls.py
+++ courses/urls.py
@@ -11,12 +11,12 @@ urlpatterns = [
     path('api/account/', views.AccountView.as_view()),
 
     path('api/lecturer/', views.LecturerListView.as_view()),
-    path('api/lecturer/<int:pk>', views.LecturerDetailView.as_view(), name='lecturer-detail'),
+    path('api/lecturer/<int:pk>/', views.LecturerDetailView.as_view(), name='lecturer-detail'),
 
     path('api/lesson/', views.LessonListView.as_view()),
-    path('api/lesson/<int:pk>', views.LessonDetailView.as_view(), name='lesson-detail'),
+    path('api/lesson/<int:pk>/', views.LessonDetailView.as_view(), name='lesson-detail'),
 
     path('api/course/', views.CourseListView.as_view()),
-    path('api/course/<int:pk>', views.CourseDetailView.as_view(), name='course-detail'),
-    path('api/course/<int:pk>/register', views.RegisterOnCourseView.as_view(), name='register-on-course'),
+    path('api/course/<int:pk>/', views.CourseDetailView.as_view(), name='course-detail'),
+    path('api/course/<int:pk>/register/', views.RegisterOnCourseView.as_view(), name='register-on-course'),
 ]
